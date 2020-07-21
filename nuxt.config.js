aea4f5e52894e13fc24557661697a375f33235c7
Yunus Morkramer <yunus@morkramer.com>
2020-07-21 14:22:44 +0200

    Changed title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'INAIA CRM',
+    title: 'CRM - INAIA Cloud',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
