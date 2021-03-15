9a24a0058797e38543a8b0461e04aa8737806ef6
Nur Hossain <shahid9829@gmail.com>
2021-03-15 18:30:43 +0600

    updating configs for production-build

--- config.js
+++ config.js
@@ -55,20 +55,20 @@ export default {
 
     production: {
         axios: {
-            baseURL: 'https://auth-staging.inaia.cloud/api/v1'
+            baseURL: 'https://auth.inaia.cloud/api/v1'
         },
         env: {
-            universalLogin: 'https://login-staging.inaia.cloud',
+            universalLogin: 'https://login.inaia.cloud',
             dashboardPath: '/dashboard',
-            s3BucketUri: 'https://s3.eu-central-1.amazonaws.com/staging-storage.inaia.cloud/',
-            // s3BucketUri: 'https://staging-storage.inaia.cloud.s3.amazonaws.com/',
+            s3BucketUri: 'https://s3.eu-central-1.amazonaws.com/storage.inaia.cloud/',
+            // s3BucketUri: 'https://storage.inaia.cloud.s3.amazonaws.com/',
 
-            productApiUrl: 'https://products-staging.inaia.cloud/api/v1',
+            productApiUrl: 'https://products.inaia.cloud/api/v1',
 
             entryPoints: {
-                crm: 'https://crm-staging.inaia.cloud',
-                admin: 'https://admin-staging.inaia.cloud',
-                golddinar: 'https://golddinar-admin-staging.inaia.cloud'
+                crm: 'https://crm.inaia.cloud',
+                admin: 'https://admin.inaia.cloud',
+                golddinar: 'https://golddinar-admin.inaia.cloud'
             }
         },
         server: {
