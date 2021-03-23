a66f2c69964b1b47b28f961cf0aa8d30e11eec59
Nur Hossain <shahid9829@gmail.com>
2021-03-23 23:15:01 +0600

    fixing api-endpoint for customers

--- store/clients.js
+++ store/clients.js
@@ -86,7 +86,7 @@ export const actions = {
     },
     initClientData(context, payload) {
         return this.$axios
-            .get(`/contacts?include=account,type,person_data,address,country,channels${ payload }`)
+            .get(`/contacts/customers?include=account,type,person_data,address,country,channels${ payload }`)
             .then(response => {
                 const clientData = response.data.data
                 context.commit('initClientData', clientData)
