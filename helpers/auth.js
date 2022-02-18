4b49749e27532f2be6c07d0b9fddd706abc77d2e
Nur Hossain <shahid9829@gmail.com>
2022-02-18 23:53:27 +0000

    IC-13: sending Token via POST method

--- helpers/auth.js
+++ helpers/auth.js
@@ -110,3 +110,18 @@ export function holdingActiveRoleIds(holdingRoleIds, availableRoles) {
 export function getEmployeeTypeId(types) {
     return types['employee']
 }
+
+export function redirectPost(url, data) {
+    var form = document.createElement('form');
+    document.body.appendChild(form);
+    form.method = 'post';
+    form.action = url;
+    for (var name in data) {
+        var input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = name;
+        input.value = data[name];
+        form.appendChild(input);
+    }
+    form.submit();
+}
