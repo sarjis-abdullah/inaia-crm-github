4a09e8dbc080fe944b13879193daa355b0ffd849
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2022-03-28 13:09:36 +0100

    enable admin to get user latest activities

---
+++ components/Contacts/LatestTransactions.vue
@@ -0,0 +1,11 @@
+<template>
+    
+</template>
+<script>
+import { mapGetters } from "vuex";
+import { Table, TableColumn, DropdownMenu, DropdownItem, Dropdown } from 'element-ui';
+
+</script>
+<style scoped>
+
+</style>
