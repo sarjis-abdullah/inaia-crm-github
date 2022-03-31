8817b36816a48622dbc112372871a4fd1bc2bb15
Yunus Morkramer <yunusmorkramer@Yunuss-iMac.local>
2022-03-31 10:56:10 +0200

    Fixed TransactionStatus

--- components/Banking/TransactionStatus.vue
+++ components/Banking/TransactionStatus.vue
@@ -1,6 +1,6 @@
 <template>
-  <Badge type="success" v-if="status=='APPROVED' || lifecycle_status=='COMPLETE'"><slot>{{ $t('completed')}}</slot></Badge>
-  <Badge type="secondary" v-else-if="status=='APPROVED' || lifecycle_status=='AUTHORISED'"><slot>{{ $t('pending')}}</slot></Badge>
+  <Badge type="success" v-if="status=='APPROVED' && lifecycle_status=='COMPLETE'"><slot>{{ $t('completed')}}</slot></Badge>
+  <Badge type="secondary" v-else-if="status=='APPROVED' && lifecycle_status=='AUTHORISED'"><slot>{{ $t('pending')}}</slot></Badge>
   <Badge type="danger" v-else-if="status=='DECLINED'"><slot>{{ $t('rejected')}}</slot></Badge>
   <Badge type="default" v-else><slot>{{ status + ' / ' + lifecycle_status }}</slot></Badge>
 </template>
