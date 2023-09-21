78de60c3d519950433a1806363cf7323f01f86de
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2023-09-21 16:55:07 +0100

    update saving plan payment information,show meta information, show more details in purchase orders

---
+++ components/common/MetaInfo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div  class="text-sm-left text-muted" v-if="meta">
+        {{ meta.from }} - {{ meta.to }} {{ $t('of') }} {{ meta.total }}
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        meta:{
+            type: Object,
+            default:null
+        }
+    }
+}
+</script>
