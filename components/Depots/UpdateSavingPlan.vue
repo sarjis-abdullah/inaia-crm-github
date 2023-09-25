413daef51c1fd77fd3193582ece019ab8097268b
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2023-09-25 10:42:03 +0100

    fix conditon of updating contract

--- components/Depots/UpdateSavingPlan.vue
+++ components/Depots/UpdateSavingPlan.vue
@@ -146,7 +146,7 @@
                 {{$t('cancel')}}
             </base-button>
             <base-button type="primary" @click="() => saveSavingPlan()"
-            :disabled="isSubmitting || (checkChangeTargetAmount || (selectePaymentMethod == 'bank_account' && !selectedBankAccount))">
+            :disabled="isSubmitting || (addedAmount!=null && isNaN(addedAmount)) || ((selectePaymentMethod == 'bank_account' && !selectedBankAccount))">
                 {{$t('save')}}
             </base-button>
         </template>
