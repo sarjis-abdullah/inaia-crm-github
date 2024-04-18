061ac0b94c175bb82b9fa56adf51f833345777b2
Mahdi Njim <mahdinjim@Mahdis-MacBook-Pro.local>
2024-04-18 09:37:21 +0100

    modify sales advisor access rights

--- permissions/supportTicket.js
+++ permissions/supportTicket.js
@@ -1,11 +1,11 @@
 import userPermissions from './loggedInUserPermissions';
-import {adminEdit,adminView,crmView,crmEdit,SalesAdvisorView} from './permissions'
+import {adminEdit,adminView,crmView,crmEdit,SalesAdvisorView,supportEdit,supportView} from './permissions'
 
 export const canViewSupportTicket=()=>{
     const permissions = userPermissions();
-    return (permissions.indexOf(adminView)>-1 || permissions.indexOf(SalesAdvisorView)>-1 || permissions.indexOf(crmView) > -1);
+    return (permissions.indexOf(adminView)>-1 || permissions.indexOf(SalesAdvisorView)>-1 || permissions.indexOf(crmView) > -1  || permissions.indexOf(supportView)>-1);
 }
 export const canEditSupportTicket=()=>{
     const permissions = userPermissions();
-    return (permissions.indexOf(adminEdit)>-1 || permissions.indexOf(crmEdit)>-1);
+    return (permissions.indexOf(adminEdit)>-1 || permissions.indexOf(crmEdit)>-1 || permissions.indexOf(SalesAdvisorView)>-1 || permissions.indexOf(supportEdit)>-1);
 }
