49f175759b231ab536ec6a59c75b04eca38f76e8
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-06 17:30:43 +0600

    minor

--- helpers/address.js
+++ helpers/address.js
@@ -2,10 +2,10 @@ export function getFormattedAddress(address) {
   let text = "";
   if (address) {
     if (address.line1) {
-      text += address.line1;
+      text += address.line1 + " "
     }
     if (address.line2) {
-      text += " " + address.line2;
+      text += address.line2;
     }
     text +=
       (address.city ? ", " + address.city : "") +
