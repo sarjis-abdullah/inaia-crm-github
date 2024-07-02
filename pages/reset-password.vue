5ceefa2ef51d7cd4ee0c83d89975032c940300e0
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-07-02 16:52:40 +0600

    layout and design updated

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -1,165 +1,179 @@
 <template>
-  <div
-    class="d-flex min-h-full flex-1 flex-col justify-content-center px-3 py-12 lg:px-8"
-  >
-    <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <nuxt-link to="/">
-        <img
-          class="d-flex mx-auto w-36 h-auto"
-          src="~/static/logo.png"
-          alt="INAIA GmbH"
-        />
-      </nuxt-link>
+  <div>
+    <div class="header py-7" :class="background">
+      <div class="container">
+        <div class="header-body text-center mt-4 mb-6">
+          <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+              <h1 class="text-white">{{ $t("reset_password_title") }}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
+    </div>
+    <div class="container mt--8 pb-5 new-px-8">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7 card bg-secondary">
+          <img
+            src="~/static/pinscreen.jpg"
+            alt="pin"
+            class="d-flex w-28 h-auto mt-4 mx-auto"
+          />
 
-      <div class="bg-white py-4 mt-10 mb-10 px-4 shadow sm:rounded-lg sm:px-10">
-        <h2 class="text-center text-2xl font-bold leading-9 tracking-tight">
-          {{ $t("reset_password_title") }}
-        </h2>
-        <img
-          src="~/static/pinscreen.jpg"
-          alt="pin"
-          class="d-flex w-32 h-auto my-4 mx-auto"
-        />
-
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <div v-if="!isLoading && state.email && stillValid">
-            <validation-observer
-              ref="observer"
-              v-slot="{ invalid, handleSubmit }"
-            >
-              <div v-if="failed" class="text-center text-danger mb-2">
-                {{ failed }}
-              </div>
-
-              <form
-                class="space-y-6"
-                @submit.prevent="handleSubmit(resetPassword)"
-                v-if="!success"
+          <div class="card-body px-lg-5">
+            <div v-if="!isLoading && state.email && stillValid">
+              <validation-observer
+                ref="observer"
+                v-slot="{ invalid, handleSubmit }"
               >
-                <div>
-                  <div class="mt-1">
-                    <base-input
-                      alternative
-                      class="mb-3"
-                      name="email"
-                      id="email"
-                      rules="required"
-                      prepend-icon="fas fa-envelope"
-                      placeholder="e.g. Email address"
-                      v-model="state.email"
-                      disabled
-                    />
-                  </div>
+                <div v-if="failed" class="text-center text-danger mb-2">
+                  {{ failed }}
                 </div>
 
-                <div>
-                  <div class="mt-1">
-                    <base-input
-                      alternative
-                      class="mb-3"
-                      name="password"
-                      :rules="{
-                        required: true,
-                        min: 8,
-                        special_character: true,
-                      }"
-                      prepend-icon="ni ni-lock-circle-open"
-                      type="password"
-                      placeholder="Password"
-                      v-model="state.password"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <div class="mt-1">
-                    <base-input
-                      alternative
-                      class="mb-3"
-                      name="password"
-                      :rules="{
-                        required: true,
-                        min: 8,
-                        special_character: true,
-                      }"
-                      prepend-icon="ni ni-lock-circle-open"
-                      type="password"
-                      placeholder="Confirm password"
-                      v-model="state.confirmPassword"
-                    />
-                  </div>
-                </div>
-                <span class="invalid-feedback" style="display: block">{{
-                  showConfirmPasswordError
-                    ? $t("confirm_password_mismatch")
-                    : ""
-                }}</span>
-
-                <div class="d-flex justify-content-center">
-                  <base-button
-                    :disabled="
-                      disabled || isSubmitting || showConfirmPasswordError
-                    "
-                    type="primary"
-                    native-type="submit"
-                    class="my-4 w-full"
-                  >
-                    {{ $t("confirm") }}
-                    <i v-if="isSubmitting" class="fas fa-spinner fa-spin"></i>
-                  </base-button>
-                </div>
-              </form>
-            </validation-observer>
-            <div
-              class="mt-1"
-              v-if="success"
-            >
-              <div
-                class="mt-1 d-flex align-items-center justify-content-center"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                  data-slot="icon"
-                  class="h-8 w-8 text-success"
+                <form
+                  class="space-y-6"
+                  @submit.prevent="handleSubmit(resetPassword)"
+                  v-if="!success"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span class="ml-2 text-success">
-                  {{ $t("password_reset_with_success") }}
-                </span>
-              </div>
-              <div class="mt-2 d-flex justify-content-center">
-                <router-link to="/" class=""><small>
-                  {{ $t('back_to_login') }}
-                </small></router-link>
+                  <div>
+                    <div class="mt-1">
+                      <base-input
+                        alternative
+                        class="mb-3"
+                        name="email"
+                        id="email"
+                        rules="required"
+                        prepend-icon="fas fa-envelope"
+                        placeholder="e.g. Email address"
+                        v-model="state.email"
+                        disabled
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="mt-1">
+                      <base-input
+                        alternative
+                        class="mb-3"
+                        name="password"
+                        :rules="{
+                          required: true,
+                          min: 8,
+                          special_character: true,
+                        }"
+                        prepend-icon="ni ni-lock-circle-open"
+                        type="password"
+                        placeholder="Password"
+                        v-model="state.password"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="mt-1">
+                      <base-input
+                        alternative
+                        class="mb-3"
+                        name="password"
+                        :rules="{
+                          required: true,
+                          min: 8,
+                          special_character: true,
+                        }"
+                        prepend-icon="ni ni-lock-circle-open"
+                        type="password"
+                        placeholder="Confirm password"
+                        v-model="state.confirmPassword"
+                      />
+                    </div>
+                  </div>
+                  <span class="invalid-feedback" style="display: block">{{
+                    showConfirmPasswordError
+                      ? $t("confirm_password_mismatch")
+                      : ""
+                  }}</span>
+
+                  <div class="d-flex justify-content-center">
+                    <base-button
+                      :disabled="
+                        disabled || isSubmitting || showConfirmPasswordError
+                      "
+                      type="primary"
+                      native-type="submit"
+                      class="my-4 w-full"
+                    >
+                      {{ $t("confirm") }}
+                      <i v-if="isSubmitting" class="fas fa-spinner fa-spin"></i>
+                    </base-button>
+                  </div>
+                </form>
+              </validation-observer>
+              <div class="mt-1" v-if="success">
+                <div
+                  class="mt-1 d-flex align-items-center justify-content-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    data-slot="icon"
+                    class="h-8 w-8 text-success"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="ml-2 text-success">
+                    {{ $t("password_reset_with_success") }}
+                  </span>
+                </div>
+                <div class="mt-2 d-flex justify-content-center">
+                  <router-link to="/" class=""
+                    ><small>
+                      {{ $t("back_to_login") }}
+                    </small></router-link
+                  >
+                </div>
               </div>
             </div>
-          </div>
-          <div v-else-if="isLoading">
-            <p class="text-center">{{ $t("checking") }}...</p>
-            <div class="d-flex justify-content-center mt-6">
-              <Loader />
+            <div v-else-if="isLoading">
+              <p class="text-center">{{ $t("checking") }}...</p>
+              <div class="d-flex justify-content-center mt-6">
+                <Loader />
+              </div>
             </div>
-          </div>
-          <div
-            v-else-if="!isLoading && (!state.email || !token || !stillValid)"
-          >
-            <p class="text-center">
-              {{ $t("requestNewLink") }}
-            </p>
-            <div class="d-flex justify-content-center mt-6">
-              <NuxtLink
-                to="/?forgot-password=1"
-                class="cursor-pointer font-semibold"
-                >{{ $t("clickHere") }}</NuxtLink
-              >
+            <div
+              v-else-if="!isLoading && (!state.email || !token || !stillValid)"
+            >
+              <p class="text-center">
+                {{ $t("requestNewLink") }}
+              </p>
+              <div class="d-flex justify-content-center mt-1">
+                <NuxtLink
+                  to="/request-password"
+                  class="cursor-pointer font-semibold"
+                  >{{ $t("clickHere") }}</NuxtLink
+                >
+              </div>
             </div>
           </div>
         </div>
@@ -170,9 +184,10 @@
 
 <script>
 import Loader from "@/components/common/Loader/Loader.vue";
+import appNames from "../appNames";
 
 export default {
-  layout: "no-layout",
+  layout: "AuthLayout",
   components: {
     Loader,
   },
@@ -202,6 +217,15 @@ export default {
         return false;
       }
       return true;
+    },
+    background() {
+      let app = process.env.CURRENT_APP;
+      let env = process.env.DEPLOYMENT_ENV;
+      if (env != "production") {
+        return "bg-gradient-warning";
+      }
+      if (app == appNames.getGreenGold) return "bg-gradient-default";
+      else return "bg-gradient-info";
     },
   },
   mounted() {
@@ -259,63 +283,15 @@ export default {
 </script>
 
 <style scoped>
-.min-h-full {
-  min-height: 100%;
-}
-.flex-1 {
-  flex: 1 1 0%;
-}
-.flex-col {
-  flex-direction: column;
-}
-.py-12 {
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-}
-.lg\:px-8 {
-  padding-left: 2rem;
-  padding-right: 2rem;
-}
-
 .mx-auto {
   margin-left: auto;
   margin-right: auto;
 }
-.w-36 {
-  width: 9rem;
-}
-.w-32 {
-  width: 8rem;
+.w-28 {
+  width: 7rem;
 }
 .h-auto {
   height: auto;
-}
-.bg-white {
-  background-color: #ffffff;
-}
-.py-8 {
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-}
-.mt-10 {
-  margin-top: 2.5rem;
-}
-.mb-10 {
-  margin-bottom: 2.5rem;
-}
-.px-4 {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-.shadow {
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-}
-.sm\:rounded-lg {
-  border-radius: 0.5rem;
-}
-.sm\:px-10 {
-  padding-left: 2.5rem;
-  padding-right: 2.5rem;
 }
 .text-2xl {
   font-size: 1.5rem;
@@ -324,13 +300,6 @@ export default {
 .font-bold {
   font-weight: 700;
 }
-.leading-9 {
-  line-height: 2.25rem;
-}
-.tracking-tight {
-  letter-spacing: -0.025em;
-}
-
 .space-y-6 > :not([hidden]) ~ :not([hidden]) {
   margin-top: 1.5rem;
 }
@@ -345,36 +314,20 @@ export default {
   padding-top: 0.375rem;
   padding-bottom: 0.375rem;
 }
-.shadow-sm {
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-}
 .cursor-pointer {
   cursor: pointer;
 }
 .font-semibold {
   font-weight: 600;
 }
-
-@media (min-width: 640px) {
-  .sm\:w-full {
-    width: 100%;
-  }
-}
-@media (min-width: 640px) {
-  .sm\:mx-auto {
-    margin-left: auto;
-    margin-right: auto;
-  }
-}
-@media (min-width: 640px) {
-  .sm\:max-w-md {
-    max-width: 28rem;
-  }
-}
 .h-8 {
   height: 2rem;
 }
 .w-8 {
   width: 2rem;
+}
+.new-px-8 {
+  padding-left: 2rem;
+  padding-right: 2rem;
 }
 </style>
