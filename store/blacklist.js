15ba4a75c39cbaafdb9fa16422527fcfe48d4ad0
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-07-26 17:24:59 +0600

    store updated

---
+++ store/blacklist.js
@@ -0,0 +1,27 @@
+export const state = () => {
+    return {
+    }
+}
+
+const initialState  = state()
+
+export const getters = {
+}
+export const mutations = {
+}
+export const actions = {
+    fetchData(context, payload) {
+        return this.$axios
+            .get(`/blacklist${payload}`)
+            .then(response => {
+                return response
+            })
+    },
+    remove(context, payload) {
+        return this.$axios
+            .delete(`/blacklist/${ payload }`)
+            .then(response => {
+                return response
+            })
+    }
+}
