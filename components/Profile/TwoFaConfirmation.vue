67d20f159248486129060247d512018ffbf791c9
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-08-13 17:47:30 +0600

    more design updated

--- components/Profile/TwoFaConfirmation.vue
+++ components/Profile/TwoFaConfirmation.vue
@@ -1,134 +1,85 @@
 <template>
   <modal :show.sync="show" :show-close="true" modal-classes="modal-secondary">
     <template #header>
-      <h5 class="modal-title">Select Profile Photo</h5>
+      <h5>
+        {{
+          hasTwoFaEnabled
+            ? $t("confirm_disable_two_factor_authentication")
+            : $t("confirm_enable_two_factor_authentication")
+        }}
+        <p class="text-sm text-gray-500" v-if="hasTwoFaEnabled">
+          {{ $t("do_you_want_to_disable_two_factor_authentication?") }}
+        </p>
+        <p class="text-sm text-gray-500" v-else>
+          {{ $t("do_you_want_to_enable_two_factor_authentication?") }}
+        </p>
+      </h5>
     </template>
-    <div class="modal-content">
-      <div class="modal-header">
-        <div class="modal-title">
-          <div class="d-flex">
-            <div
-              class="d-flex justify-content-center rounded-circle bg-danger text-white"
-              style="width: 40px; height: 40px"
-            >
-              <ExclamationTriangleIcon
-                class="w-50 h-50 text-primary"
-                aria-hidden="true"
-              />
-            </div>
-            <article class="ml-3 mb-0">
-              <h5>
-                {{
-                  hasTwoFaEnabled
-                    ? $t("confirm_disable_two_factor_authentication")
-                    : $t("confirm_enable_two_factor_authentication")
-                }}
-              </h5>
-              <div class="">
-                <p class="text-sm text-gray-500" v-if="hasTwoFaEnabled">
-                  {{ $t("do_you_want_to_disable_two_factor_authentication?") }}
-                </p>
-                <p class="text-sm text-gray-500" v-else>
-                  {{ $t("do_you_want_to_enable_two_factor_authentication?") }}
-                </p>
-              </div>
-            </article>
-          </div>
-        </div>
+    <div class="" v-if="isLoading || serverErrorMsg">
+      <div v-if="isLoading" class="text-center">
+        <Loading />
       </div>
-      <div class="modal-body">
-        <div v-if="isLoading" class="text-center">
-          <Loading />
+      <div v-else>
+        <div v-if="serverErrorMsg" class="alert alert-danger">
+          <ExclamationTriangleIcon class="mr-2" />
+          {{ serverErrorMsg }}
         </div>
-        <div v-else>
-          <div v-if="serverErrorMsg" class="alert alert-danger">
-            <ExclamationTriangleIcon class="mr-2" />
-            {{ serverErrorMsg }}
-          </div>
-          <div v-if="svgContent" v-html="svgContent"></div>
+        <!-- <div v-if="svgContent" v-html="svgContent"></div>
           <code-inputs v-if="showCodeInput" @confirm="confirmDisableTwofa" />
           <check-circle-icon v-if="!hasTwoFaEnabled" @click="confrim" />
-          <check-circle-icon v-if="hasTwoFaEnabled" @click="cancel" />
-        </div>
-      </div>
-      <div class="modal-footer">
-        <div
-          v-if="hasTwoFaEnabled && !isLoading && showCodeInput"
-          class="w-100"
-        >
-          <div class="alert alert-warning text-center">
-            {{ $t("enter_6_digit_code_to_confirm_disable_two_fa") }}
-          </div>
-          <div class="d-flex justify-content-center">
-            <CodeInputs @complete="confirmDisableTwofa" :length="6" />
-          </div>
-          <div class="alert alert-warning text-center mt-4">
-            {{ $t("two_fa_disable_warning_message") }}
-          </div>
-        </div>
-        <div
-          v-else-if="!isLoading && svgContent && !hasTwoFaEnabled"
-          class="w-100"
-        >
-          <div class="text-center">
-            <p>
-              {{ $t("scan_the_qr_code_below_with_your_authenticator_app") }}
-            </p>
-            <picture>
-              <div v-html="svgContent" v-if="svgContent"></div>
-            </picture>
-            <div class="alert alert-success mt-3">
-              <check-circle-icon class="mr-2" />
-              {{ $t("two_factor_authentication_enabled_success") }}
-            </div>
-            <div>
-              <p>{{ $t("enter_6_digit_code_prompt") }}</p>
-              <p>{{ $t("keep_mobile_device_secure") }}</p>
-            </div>
-          </div>
-        </div>
-
-        <button
-          v-if="!svgContent"
-          type="button"
-          :disabled="isLoading"
-          class="btn btn-secondary"
-          @click="cancel"
-          ref="cancelButtonRef"
-        >
-          {{ $t("cancel") }}
-        </button>
-        <button
-          v-if="!hasTwoFaEnabled"
-          type="button"
-          :disabled="isLoading"
-          class="btn btn-primary"
-          @click="svgContent ? enable() : confrim()"
-        >
-          {{ svgContent && !isLoading ? $t("ok") : $t("confirm") }}
-        </button>
+          <check-circle-icon v-if="hasTwoFaEnabled" @click="cancel" /> -->
       </div>
     </div>
-    <!-- <template #footer>
+    <div v-if="hasTwoFaEnabled && !isLoading && showCodeInput" class="w-100">
+      <div class="alert alert-warning text-center">
+        {{ $t("enter_6_digit_code_to_confirm_disable_two_fa") }}
+      </div>
+      <div class="d-flex justify-content-center">
+        <CodeInputs @complete="confirmDisableTwofa" :length="6" />
+      </div>
+      <div class="alert alert-warning text-center mt-4">
+        {{ $t("two_fa_disable_warning_message") }}
+      </div>
+    </div>
+    <div v-else-if="!isLoading && svgContent && !hasTwoFaEnabled" class="w-100">
+      <div class="text-center">
+        <p>
+          {{ $t("scan_the_qr_code_below_with_your_authenticator_app") }}
+        </p>
+        <picture>
+          <div v-html="svgContent" v-if="svgContent"></div>
+        </picture>
+        <div class="alert alert-success mt-3">
+          <check-circle-icon class="mr-2" />
+          {{ $t("two_factor_authentication_enabled_success") }}
+        </div>
+        <div>
+          <p>{{ $t("enter_6_digit_code_prompt") }}</p>
+          <p>{{ $t("keep_mobile_device_secure") }}</p>
+        </div>
+      </div>
+    </div>
+    <template #footer>
       <button
+        v-if="!svgContent"
         type="button"
+        :disabled="isLoading"
+        class="btn btn-secondary"
+        @click="cancel"
+        ref="cancelButtonRef"
+      >
+        {{ $t("cancel") }}
+      </button>
+      <button
+        v-if="!hasTwoFaEnabled"
+        type="button"
+        :disabled="isLoading"
         class="btn btn-primary"
-        @click.prevent.stop="submitUpload"
-        :disabled="startProcess || !photo"
+        @click="svgContent ? enable() : confrim()"
       >
-        Save photo
+        {{ svgContent && !isLoading ? $t("ok") : $t("confirm") }}
       </button>
-      <button
-        type="button"
-        class="btn btn-link ml-auto"
-        @click.prevent.stop="
-          clicked = !clicked;
-          photo = null;
-        "
-      >
-        Close
-      </button>
-    </template> -->
+    </template>
   </modal>
 </template>
 <script>
@@ -212,11 +163,13 @@ export default {
     },
     async confrim() {
       try {
+        this.serverErrorMsg = "";
         this.isLoading = true;
         this.showCodeInput = false;
-        const res = this.$axios.post(`mfa/init`)
+        const res = await this.$axios.post(`mfa/init`);
+        console.log(res, "res");
         // const res = await AccountService.enableTwoFA();
-        this.svgContent = res.qrCode;
+        this.svgContent = res.data.qrCode;
         // await this.loadAccount();
       } catch (error) {
         this.serverErrorMsg = error.message ?? "";
@@ -246,3 +199,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.modal-dialog {
+  max-width: 600px;
+}
+</style>
