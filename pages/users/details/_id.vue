f1abe72d8338344e3bd4badef48133abfa010f8c
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-09-11 16:56:24 +0600

    un-necessary component removed

--- pages/users/details/_id.vue
+++ pages/users/details/_id.vue
@@ -1,13 +1,58 @@
 <template>
-    <AccountDetails />
-</template>
-<script>
-import AccountDetails from "@/components/Users/AccountDetails"
-
-export default {
+    <div>
+      <base-header class="pb-5">
+        <div class="row align-items-center py-4">
+          <div class="col-lg-6 col-7">
+          </div>
+        </div>
+      </base-header>
+  
+      <div class="container-fluid mt--5">
+        <div class="row">
+          <div class="col-lg-8">
+            <AccountDetails :resource="singleAccountUser" />
+          </div>
+  
+          <div class="col-lg-4"></div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { mapGetters } from "vuex";
+  import AccountDetails from "@/components/Contacts/AccountDetails";
+  export default {
     layout: 'DashboardLayout',
     components: {
-        AccountDetails
-    }
-}
-</script>
+      AccountDetails,
+    },  
+    computed: {
+      ...mapGetters({
+        resource: "clients/singleAccountData",
+      }),
+      accountUserId() {
+        return this.$route.params.id;
+      },
+      singleAccountUser() {
+        return this.resource && this.resource.customer;
+      },
+    },
+    watch: {
+      accountUserId: {
+        handler() {
+          if (this.accountUserId) {
+            this.initClientData();
+          }
+        },
+        immediate: true,
+      },
+    },
+    methods: {
+      initClientData() {
+        this.$store.dispatch("clients/accountDetailsData", this.accountUserId);
+      },
+    },
+  };
+  </script>
+  
